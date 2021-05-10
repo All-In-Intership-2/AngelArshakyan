729caea716a659775760c7102d3ba1c625a06ee6
arshakyana-1619 <80577020+arshakyana-1619@users.noreply.github.com>
2021-05-10 19:57:27 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function fun(){
+    var a = document.querySelector("#menu");
+    a.style.display = "block";
+    var b = document.querySelector("body");
+    b.style.overflow = "hidden"
+   
+
+}
+
+function fun1(){
+    var b = document.querySelector("#menu");
+    b.style.display = "none";
+    var a = document.querySelector("body");
+    a.style.overflow = "visible";
+}
+
